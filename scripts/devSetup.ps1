5ae11b25629a21067921e18e8aa2490ab15deeea
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2022-12-03 15:21:03 -0600

    dev setup - remove vsc ext install

--- scripts/devSetup.ps1
+++ scripts/devSetup.ps1
@@ -9,13 +9,3 @@ if (Get-Command choco -CommandType Application) {
     choco install vscode-insiders --yes --no-progress
     choco install discord.install --yes --no-progress
 }
-
-if (Get-Command code-insiders -CommandType Application) {
-    code-insiders --install-extension 'streetsidesoftware.code-spell-checker'
-    code-insiders --install-extension 'usernamehw.errorlens'
-    code-insiders --install-extension 'DavidAnson.vscode-markdownlint'
-    code-insiders --install-extension 'sdras.night-owl'
-    code-insiders --install-extension 'TylerLeonhardt.vscode-pester-test-adapter'
-    code-insiders --install-extension 'ms-dotnettools.dotnet-interactive-vscode'
-    code-insiders --install-extension 'ms-vscode.powershell-preview'
-}
