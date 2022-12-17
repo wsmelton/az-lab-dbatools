dbee3c18667d60c7bed0af20646be554b99be6c3
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2022-12-17 15:16:34 -0600

    add install GitHub CLI

--- scripts/devSetup.ps1
+++ scripts/devSetup.ps1
@@ -5,6 +5,7 @@ Start-Sleep -Seconds 15
 
 if (Get-Command choco -CommandType Application) {
     choco install git.install --yes --no-progress
+    choco install gh --yes --no-progress
     choco install pwsh --yes --no-progress
     choco install vscode-insiders --yes --no-progress
     choco install discord.install --yes --no-progress
